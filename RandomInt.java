fdb7cca1593af6592d3c9945b35cf20bd531152c
Patrick Grady <pmgrady2@linux-a2.ews.illinois.edu>
2016-02-26 15:20:27 -0600

    first commit

---
+++ RandomInt.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+ *  Compilation:  javac RandomInt.java
+ *  Execution:    java RandomInt N
+ *  
+ *  Prints a pseudo-random integer between 0 and N-1.
+ *
+ ******************************************************************************/
+public class RandomInt { 
+    public static void main(String[] args) { 
+        int N = Integer.parseInt(args[0]);
+
+        // a pseudo-random real between 0.0 and 1.0
+        double r = Math.random(); 
+
+        // a pseudo-random integer between 0 and N-1
+        int n = (int) (r * N);
+
+        System.out.println("Your random integer is: " + n);
+    }
+}
